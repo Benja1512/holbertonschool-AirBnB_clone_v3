83b1a9287ad4273886c2318b31fc4b1e2bc782a1
Benja1512 <bontiveros15@gmail.com>
2024-04-21 15:56:54 -0500

    fixed: index.py:Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 """Flask application that retrieves information"""
-from api.v1.views import app_views
-from flask import jsonify
-
-
-@app_views.route("/status", strict_slashes=False)
-def status():
-    """Returns the app status"""
-    return jsonify({"status": "OK"})
-
-
-#!/usr/bin/python3
-"""Flask application that retrieves information"""
 
 
 @app_views.route("/status", strict_slashes=False)
