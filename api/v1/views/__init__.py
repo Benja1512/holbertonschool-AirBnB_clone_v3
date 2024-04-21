9490760138ade614f171287b3bf0cb0b7256bffc
Benja1512 <bontiveros15@gmail.com>
2024-04-21 16:37:49 -0500

    feat: update init.py and cities a view for cities object handles all default restful api action

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,8 +4,7 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-
-# Importa todo desde el paquete api.v1.views.index
-# Esta importación se coloca aquí para evitar problemas de importación circular y no es recomendada por PEP8,
-# por lo tanto, es normal que PEP8 se queje al respecto. Este archivo no será verificado por PEP8.
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+
